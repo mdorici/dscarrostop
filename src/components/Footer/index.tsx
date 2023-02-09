fdb56f0e3d54de43a04c5f0a14e50e7231cd537c
mdorici <marianadorici@gmail.com>
2023-02-08 23:20:55 -0300

    Update css style

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,14 +1,22 @@
-import './styles.css';
+import "./styles.css";
 
 export default function Footer() {
-    return(
-        <>
-            <footer>
-                <div className="dsct-container">
-                    <p><i>@dscarrostop</i></p>
-                    <p><i>Rua da Pipoca, 150</i></p>
-                </div>
-            </footer>
-        </>
-    );
+  return (
+    <>
+      <footer>
+        <div className="dsct-container">
+          <div className="dsct-media">
+            <p>
+              <i>@dscarrostop</i>
+            </p>
+          </div>
+          <div className="dsct-address">
+            <p>
+              <i>Rua da Pipoca, 150</i>
+            </p>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
